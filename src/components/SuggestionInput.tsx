60ece10121b19e53131fddb0599e90cfed3f68d4
Daniel Constantin <hello@danielconstantin.net>
2024-02-07 16:25:57 +0200

    Make search results a dropdown not inline

--- src/components/SuggestionInput.tsx
+++ src/components/SuggestionInput.tsx
@@ -37,8 +37,9 @@ const SuggestionInput = ({ eventsIds }: { eventsIds: string[] }) => {
   }
   return (
     <div>
-      <div className="relative">
+      <div className="dropdown relative w-full">
         <input
+          role="button"
           placeholder="Search...."
           className="bg-background border border-[#222326] w-full py-[28px] px-[32px] outline-none focus:border-transparent z-50 relative"
           value={inputValue}
@@ -47,38 +48,36 @@ const SuggestionInput = ({ eventsIds }: { eventsIds: string[] }) => {
           onBlur={handleInputBlur}
           onKeyDown={handleKeyDown}
         />
-        <Link href={`/${inputValue}`}>
-          <Image
-            width={28}
-            height={28}
-            alt="search"
-            className="absolute right-[32px] top-1/2 -translate-y-1/2 text-[18px] z-[51]"
-            src="/search.svg"
-          />
-        </Link>
+        <Image
+          width={28}
+          height={28}
+          alt="search"
+          className="absolute right-[32px] top-1/2 -translate-y-1/2 text-[18px] z-[51]"
+          src="/search.svg"
+        />
+        <div tabIndex={0} className="dropdown-content z-50 w-full">
+          <ul className="mt-[10px] z-50 relative max-h-[200px] overflow-y-scroll">
+            {eventsIds
+              .filter((item) => item.includes(inputValue))
+              .map((suggestion) => (
+                <li
+                  key={suggestion}
+                  // onClick={() => {
+                  //   handleSelectChange(suggestion);
+                  // }}
+                  className="cursor-pointer p-[8px] bg-background flex justify-between"
+                >
+                  <Link href={`/message/${suggestion}`} className="w-full">
+                    <div className="flex justify-between w-full">
+                      <p>{truncateId(suggestion)}</p>
+                      <p>{Math.random().toFixed(5)}</p>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-      {/* {isInputFocused && inputValue !== "" && (
-        <ul className="mt-[10px] z-50 relative max-h-[200px] overflow-y-scroll">
-          {eventsIds
-            .filter((item) => item.includes(inputValue))
-            .map((suggestion) => (
-              <li
-                key={suggestion}
-                // onClick={() => {
-                //   handleSelectChange(suggestion);
-                // }}
-                className="cursor-pointer p-[8px] bg-background flex justify-between"
-              >
-                <Link href={`/${suggestion}`} className="w-full">
-                  <div className="flex justify-between w-full">
-                    <p>{truncateId(suggestion)}</p>
-                    <p>{Math.random().toFixed(5)}</p>
-                  </div>
-                </Link>
-              </li>
-            ))}
-        </ul>
-      )} */}
       {isInputFocused && (
         <div
           className={`fixed top-0 left-0 w-full h-full bg-[#0000004d] z-[1]`}
