138cbb9340245592f19788556eea8cb75948fc5c
Daniel Constantin <hello@danielconstantin.net>
2024-03-13 12:46:36 +0200

    Remove balance from user page

--- src/app/entity/[slug]/UserPage.tsx
+++ src/app/entity/[slug]/UserPage.tsx
@@ -1,13 +1,9 @@
 import React from "react"
 
 import { IdBlock } from "@/components/IdBlock"
-import { SectionInfo } from "@/components/SectionInfo"
 import EventsTable from "@/page-components/HomePage/EventsTable"
 import { getLatestAoEvents } from "@/services/aoscan"
-import { getBalance } from "@/services/token-api"
 import { normalizeAoEvent } from "@/utils/ao-event-utils"
-import { nativeTokenInfo } from "@/utils/native-token"
-import { formatNumber } from "@/utils/number-utils"
 
 export async function UserPage({ entityId }: { entityId: string }) {
   const ownerId = entityId
@@ -23,25 +19,13 @@ export async function UserPage({ entityId }: { entityId: string }) {
   )
   const initialTableData = events.map(normalizeAoEvent)
 
-  const balance = await getBalance(nativeTokenInfo, ownerId)
-
   return (
     <main className="min-h-screen mb-6">
       <div className="flex gap-2 items-center text-sm mt-12 mb-11">
-        <p className="text-[#9EA2AA] ">USER</p>
+        <p className="text-[#9EA2AA]">USER</p>
         <p className="font-bold">/</p>
         <IdBlock label={ownerId} />
       </div>
-      <SectionInfo
-        title="Balance"
-        value={
-          <IdBlock
-            label={`${formatNumber(balance)} ${nativeTokenInfo.ticker}`}
-            value={String(balance)}
-            hideTooltip
-          />
-        }
-      />
       <EventsTable
         initialData={initialTableData}
         pageSize={pageSize}
