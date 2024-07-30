b2cc31c37175b89f911c15590b3af041423ae821
micovi <micleaovidiul@gmail.com>
2024-07-30 14:00:53 +0300

    feat: replace AOCRED with AO process

--- src/utils/native-token.ts
+++ src/utils/native-token.ts
@@ -1,9 +1,9 @@
 import { TokenInfo } from "@/services/token-api"
 
 export const nativeTokenInfo: TokenInfo = {
-  processId: "Sa0iBLPNyJQrwpTTG-tWLQU-1QeUAJA73DdxGGiKoJc",
-  denomination: 3,
-  ticker: "AOCRED",
-  logo: "eIOOJiqtJucxvB4k8a-sEKcKpKTh9qQgOV3Au7jlGYc",
-  name: "AO-CRED",
+  processId: "m3PaWzK4PTG9lAaqYQPaPdOcXdO8hYqi5Fe9NWqXd0w",
+  denomination: 12,
+  ticker: "AO",
+  logo: "UkS-mdoiG8hcAClhKK8ch4ZhEzla0mCPDOix9hpdSFE",
+  name: "AO",
 }
