e3fdaf9f3853d9c9f0fb306b02a143c5070feb2c
Daniel Constantin <hello@danielconstantin.net>
2024-09-11 18:04:35 +0300

    Fix permaweb deployment

--- scripts/fund-wallet.js
+++ scripts/fund-wallet.js
@@ -3,18 +3,17 @@
 import Irys from "@irys/sdk"
 import { config } from "dotenv"
 
-config()
+config({ path: [".env", ".env.local"] })
 const DEPLOY_KEY = process.env.DEPLOY_KEY
-const ANT_CONTRACT = process.env.ARNS_ANT_CONTRACT
 
-async function deploy() {
+async function fund() {
   if (!DEPLOY_KEY) throw new Error("DEPLOY_KEY not configured")
-  if (!ANT_CONTRACT) throw new Error("ANT_CONTRACT not configured")
 
+  // https://docs.ardrive.io/docs/turbo/migrating.html#purchasing-turbo-credits-with-the-irys-sdk
   const jwk = JSON.parse(Buffer.from(DEPLOY_KEY, "base64").toString("utf-8"))
   const irys = new Irys({
     network: "mainnet",
-    // url: "https://turbo.ardrive.io",
+    url: "https://turbo.ardrive.io", // URL of the node you want to connect to, https://turbo.ardrive.io will facilitate upload using ArDrive Turbo.
     token: "arweave",
     key: jwk,
   })
@@ -22,10 +21,17 @@ async function deploy() {
   const balance = await irys.getBalance("yqRGaljOLb2IvKkYVa87Wdcc8m_4w6FI58Gej05gorA")
   console.log("📜 LOG > node balance:", irys.utils.fromAtomic(balance), irys.token)
 
-  const fundTx = await irys.fund(irys.utils.toAtomic(1))
-  console.log(
-    `📜 LOG > Successfully funded ${irys.utils.fromAtomic(fundTx.quantity)} ${irys.token}`,
-  )
+  // const withdrawTx = await irys.withdrawBalance(irys.utils.toAtomic(0.604))
+  // console.log(
+  //   `📜 LOG > Successfully withdrew ${irys.utils.fromAtomic(withdrawTx.requested)} ${irys.token}`,
+  //   withdrawTx,
+  // )
+
+  // const fundTx = await irys.fund(irys.utils.toAtomic(0.1))
+  // console.log(
+  //   `📜 LOG > Successfully funded ${irys.utils.fromAtomic(fundTx.quantity)} ${irys.token}`,
+  //   fundTx,
+  // )
 }
 
-deploy().then()
+fund().then()
