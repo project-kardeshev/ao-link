bf90e23a1f7641377302aa690b7d9e53c0d5a171
Daniel Constantin <hello@danielconstantin.net>
2024-10-21 17:04:29 +0300

    Change gateway links arweave.dev -> arweave.net

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export function Footer() {
               /
             </Typography>
             <Link
-              href="https://ao.arweave.dev"
+              href="https://ao.arweave.net"
               target="_blank"
               sx={{
                 color: "rgb(180, 180, 180)",
