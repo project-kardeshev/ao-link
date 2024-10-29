80ac8128b1feae65a9deb5f89ce53460ec27d62e
Daniel Constantin <hello@danielconstantin.net>
2024-10-29 15:41:02 +0200

    Remove token holder slicing

--- src/app/token/[slug]/TokenHolderChart.tsx
+++ src/app/token/[slug]/TokenHolderChart.tsx
@@ -2,7 +2,6 @@ import React from "react"
 
 import { PieChart } from "@/components/Charts/PieChart"
 import { TokenHolder, TokenInfo } from "@/services/token-api"
-import { nativeTokenInfo } from "@/utils/native-token"
 
 type TokenHolderChartProps = {
   data: TokenHolder[]
@@ -10,15 +9,13 @@ type TokenHolderChartProps = {
 }
 
 export function TokenHolderChart(props: TokenHolderChartProps) {
-  const { data, tokenInfo } = props
-
-  const startIndex = tokenInfo.processId === nativeTokenInfo.processId ? 2 : 0
+  const { data } = props
 
   return (
     <>
       <PieChart
         seriesTitle="Quantity"
-        data={data.slice(startIndex, 100).map((holder) => ({
+        data={data.slice(0, 100).map((holder) => ({
           name: holder.entityId,
           y: holder.balance,
         }))}
